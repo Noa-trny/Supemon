be3570eec51fdf319fc0e3b412d2e23d4d12ebb6
Noa-trny <noa.tournoy@supinfo.com>
2025-01-29 15:39:56 +0100

    feat(init 3 Supemon)

--- include/supemon.h
+++ include/supemon.h
@@ -27,4 +27,67 @@ typedef struct {
 } Supemon;
 
 
+static const Supemon SUPMANDER = {
+    .name = "Supmander",
+    .level = 1,
+    .exp = 0,
+    .hp = 10,
+    .max_hp = 10,
+    .attack = 1,
+    .base_attack = 1,
+    .defense = 1,
+    .base_defense = 1,
+    .evasion = 1,
+    .base_evasion = 1,
+    .accuracy = 2,
+    .base_accuracy = 2,
+    .speed = 1,
+    .moves = {
+        {"Scratch", 3, 0, ""},
+        {"Grawl", 0, 1, "attack"}
+    }
+};
+
+static const Supemon SUPASAUR = {
+    .name = "Supasaur",
+    .level = 1,
+    .exp = 0,
+    .hp = 9,
+    .max_hp = 9,
+    .attack = 1,
+    .base_attack = 1,
+    .defense = 1,
+    .base_defense = 1,
+    .evasion = 3,
+    .base_evasion = 3,
+    .accuracy = 2,
+    .base_accuracy = 2,
+    .speed = 2,
+    .moves = {
+        {"Pound", 2, 0, ""},
+        {"Foliage", 0, 1, "evasion"}
+    }
+};
+
+static const Supemon SUPIRTLE = {
+    .name = "Supirtle",
+    .level = 1,
+    .exp = 0,
+    .hp = 11,
+    .max_hp = 11,
+    .attack = 1,
+    .base_attack = 1,
+    .defense = 2,
+    .base_defense = 2,
+    .evasion = 2,
+    .base_evasion = 2,
+    .accuracy = 1,
+    .base_accuracy = 1,
+    .speed = 2,
+    .moves = {
+        {"Pound", 2, 0, ""},
+        {"Shell", 0, 1, "defense"}
+    }
+};
+
 #endif // SUPEMON_H
