d32921bcbe7ffebc47c75883fd8c2f4a6405b175
simon <155122848+simonpotel@users.noreply.github.com>
2025-02-03 11:18:23 +0100

    feat(supemons save)

--- main.c
+++ main.c
@@ -15,6 +15,8 @@
 void initialize_player(Player *player) {
     memset(player->name, 0, sizeof(player->name));
     player->supcoins = 0;
+    memset(player->supemons, 0, sizeof(player->supemons));
+    memset(player->selected_supemon, 0, sizeof(player->selected_supemon));
 }
 
 void choose_save(Player *player) {
@@ -60,6 +62,30 @@ void choose_save(Player *player) {
     }
 }
 
+void add_starter_supemon(Player *player, int choice) {
+    const char* starter_name;
+    switch(choice) {
+        case 1:
+            starter_name = "Supmander";
+            break;
+        case 2:
+            starter_name = "Supasaur";
+            break;
+        case 3:
+            starter_name = "Supirtle";
+            break;
+        default:
+            starter_name = "Supmander"; // invalid choice so by default supmander
+            break;
+    }
+    
+    strncpy(player->supemons[0], starter_name, sizeof(player->supemons[0]) - 1);
+    player->supemons[0][sizeof(player->supemons[0]) - 1] = '\0';
+    
+    strncpy(player->selected_supemon, starter_name, sizeof(player->selected_supemon) - 1);
+    player->selected_supemon[sizeof(player->selected_supemon) - 1] = '\0';
+}
+
 int main(void) {
     Player player; 
     char player_pseudo[50];
@@ -83,8 +109,23 @@ int main(void) {
         printf("+-----------------------------+\n");
         printf("1, 2 or 3: ");
         scanf("%d", &supemon_choice);
+        add_starter_supemon(&player, supemon_choice);
     } else {
         printf("Welcome back, %s!\n", player.name);
+        printf("\nYour Supemons:\n");
+        for (int i = 0; i < MAX_SUPEMON && player.supemons[i][0] != '\0'; i++) {
+            int valid = 1;
+            for (size_t j = 0; player.supemons[i][j] != '\0'; j++) {
+                if ((unsigned char)player.supemons[i][j] < 32 || (unsigned char)player.supemons[i][j] > 126) {
+                    valid = 0;
+                    break;
+                }
+            }
+            
+            if (valid) {
+                printf("%d - %s\n", i + 1, player.supemons[i]);
+            }
+        }
     }
 
     printf("Saving player data...\n");
--- src/save.c
+++ src/save.c
@@ -29,6 +29,16 @@ void save_player(const Player *player) {
     cJSON *player_json = cJSON_CreateObject();
     cJSON_AddNumberToObject(player_json, "supcoins", player->supcoins);
     
+    cJSON *supemons_array = cJSON_CreateArray();
+    for (int i = 0; i < MAX_SUPEMON && player->supemons[i][0] != '\0'; i++) {
+        cJSON_AddItemToArray(supemons_array, cJSON_CreateString(player->supemons[i]));
+    }
+    cJSON_AddItemToObject(player_json, "supemons", supemons_array);
+    
+    if (player->selected_supemon[0] != '\0') {
+        cJSON_AddStringToObject(player_json, "selected_supemon", player->selected_supemon);
+    }
+    
     if (cJSON_GetObjectItem(json, player->name)) {
         cJSON_ReplaceItemInObject(json, player->name, player_json);
     } else {
@@ -79,6 +89,51 @@ void load_player(Player *player, const char *name) {
             if (player_json) {
                 snprintf(player->name, sizeof(player->name), "%s", name);
                 player->supcoins = cJSON_GetObjectItem(player_json, "supcoins")->valueint;
+                
+                cJSON *supemons_array = cJSON_GetObjectItem(player_json, "supemons");
+                if (supemons_array) {
+                    int i = 0;
+                    cJSON *supemon;
+                    cJSON_ArrayForEach(supemon, supemons_array) {
+                        if (i < MAX_SUPEMON) {
+                            const char *supemon_name = supemon->valuestring;
+                            int valid = 1;
+                            for (size_t j = 0; supemon_name[j] != '\0'; j++) {
+                                if ((unsigned char)supemon_name[j] < 32 || (unsigned char)supemon_name[j] > 126) {
+                                    valid = 0;
+                                    break;
+                            }
+                            
+                            if (valid) {
+                                strncpy(player->supemons[i], supemon_name, sizeof(player->supemons[i]) - 1);
+                                player->supemons[i][sizeof(player->supemons[i]) - 1] = '\0';
+                                i++;
+                            }
+                        }
+                    }
+                    for (; i < MAX_SUPEMON; i++) {
+                        player->supemons[i][0] = '\0';
+                    }
+                }
+                
+                cJSON *selected_supemon = cJSON_GetObjectItem(player_json, "selected_supemon");
+                if (selected_supemon && selected_supemon->valuestring) {
+                    const char *selected_name = selected_supemon->valuestring;
+                    int valid = 1;
+                    for (size_t j = 0; selected_name[j] != '\0'; j++) {
+                        if ((unsigned char)selected_name[j] < 32 || (unsigned char)selected_name[j] > 126) {
+                            valid = 0;
+                            break;
+                        }
+                    }
+                    
+                    if (valid) {
+                        strncpy(player->selected_supemon, selected_name, sizeof(player->selected_supemon) - 1);
+                        player->selected_supemon[sizeof(player->selected_supemon) - 1] = '\0';
+                    } else {
+                        player->selected_supemon[0] = '\0';
+                    }
+                }
             } else {
                 printf("Player not found: %s\n", name);
             }
